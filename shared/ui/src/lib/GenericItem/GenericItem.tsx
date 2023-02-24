3bdfccee85016882ffcb854b8c3fb9c5d671e7ff
Jacob Colbeck <=>
2023-02-24 00:41:40 +0000

    Fully implemented recipe interface.

--- shared/ui/src/lib/GenericItem/GenericItem.tsx
+++ shared/ui/src/lib/GenericItem/GenericItem.tsx
@@ -1,11 +1,54 @@
 import styles from './GenericItem.module.scss';
 
 /* eslint-disable-next-line */
+
+interface Recipe {  
+    name : string,
+    localised_name : [
+      string
+    ],
+    category : string,
+    //not great but likely wont use this property
+    order : any,
+    group : {
+      name : string,
+      type : string
+    },
+    subgroup : {
+      name : string,
+      type : string
+    },
+    enabled : boolean,
+    hidden : boolean,
+    hidden_from_player_crafting : boolean,
+    emissions_multiplier : number,
+    energy : number,
+    ingredients : [
+      {
+        type : string,
+        name : string,
+        amount : number
+      }
+    ],
+    products : [
+      {
+        type: string,
+        name: string,
+        probability : number,
+        amount : number
+      }
+    ],
+    main_product : {
+      type : string,
+      name :string,
+      probability : number,
+      amount : number
+    }
+}
+
+
 export interface GenericItemProps {
-  item: {
-    name: string,
-    ingredients: []    
-  }  
+  item: Recipe
 }
 
 
@@ -14,6 +57,7 @@ export function GenericItem(props: GenericItemProps) {
     <div className={styles['genericItem']}>
      <p className={styles['genericItem__title']}>{props.item.name}</p>
      <p>{props.item.ingredients.length}</p>
+     <p>Group: {props.item.group.name}</p>
     </div>
   );
 }
